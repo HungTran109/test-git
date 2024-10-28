3c90f180912d7b3b8cd0710f6b14fe7229da97fc
Hungtd <hungtd@bytech.vn>
2024-10-28 11:56:40 +0700

    version 3

--- GD32/Project/main.c
+++ GD32/Project/main.c
@@ -99,11 +99,11 @@ int main(void)
     
     DEBUG_INFO("HELLO");
     
-    DEBUG_WARN("Version release 1.0.0\r\n");
-    DEBUG_WARN("Test amend 3\r\n");
-    DEBUG_WARN("Test amend 3\r\n");
-    DEBUG_WARN("Test amend 3\r\n");
-    DEBUG_WARN("Test amend 3\r\n");
+    DEBUG_WARN("Version release 3.0.0\r\n");
+    DEBUG_WARN("Test amend 123\r\n");
+    DEBUG_WARN("Test amend 123\r\n");
+    DEBUG_WARN("Test amend 123\r\n");
+    DEBUG_WARN("Test amend 123\r\n");
     
     DEBUG_WARN("Test amend 4\r\n");
     DEBUG_WARN("Test amend 4\r\n");
